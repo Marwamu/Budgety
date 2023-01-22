a6426e94f04ec78ecbf40cc0e049b6b360f133e2
Marwamu <marwamohamed1293@gmail.com>
2023-01-22 09:55:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var selectOption = document.getElementById('add__type')
+
+
+selectOption.addEventListener('change', function(e) {
+    
+})
+// var d = selectOption.text();
+// console.log(d);
+// $(this).next("input[type='text']").val(d);
